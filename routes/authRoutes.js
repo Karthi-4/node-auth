d403896d16ecac37ac0f837f69c42d4b6622c0ff
karthikeyan <karthirvmhss@gmail.com>
2025-03-25 17:35:24 +0530

    new

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -55,4 +55,14 @@ router.post("/logout", (req, res) => {
  res.json({ message: "Logout successful" });
 });
 
+// GET all users
+router.get("/users", async (req, res) => {
+    try {
+      const users = await User.find().select("-password"); // Exclude passwords
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ message: "Server error", error: error.message });
+    }
+  });
+
 module.exports = router;
