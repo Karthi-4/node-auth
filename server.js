59b3e07a0e84e8a6912292b010e5e779bdc4b89a
karthikeyan <karthirvmhss@gmail.com>
2025-03-25 16:46:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("./config/db");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
